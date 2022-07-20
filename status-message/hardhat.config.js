8116598eee399ad14ee723d90387cffd1b5d6d3e
Marcelo Fornet <marcelo@aurora.dev>
2022-07-20 21:57:53 +0200

    Set address of current version

--- status-message/hardhat.config.js
+++ status-message/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 const { keccak256 } = require("ethers/lib/utils");
 const { task } = require("hardhat/config");
 
-const statusMessageAddress = '';
+const statusMessageAddress = '0x8ECe66a07b872d1f0353e313a8329EcA11924815';
 
 task("deploy", "Deploy contract").addParam("nearProxyAccountId").setAction(async (taskArgs) => {
   const address = '0x' + keccak256(Buffer.from(taskArgs.nearProxyAccountId)).slice(26);
@@ -44,14 +44,15 @@ task("add-status", "Add status message").addParam("statusMessage").setAction(asy
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.13",
-  defaultNetwork: 'auroraTestnet',
+  defaultNetwork: 'aurora',
   networks: {
     aurora: {
       url: "https://mainnet.aurora.dev/",
-      accounts: ['0x1311143f626d916df89a3f8829146fe1c606b6efced2901448eff84e4853ca56']
+      accounts: ['']
     },
     auroraTestnet: {
       url: "https://testnet.aurora.dev",
+      // TODO: Some free TEST tokens :) go grab them
       accounts: ['0x1311143f626d916df89a3f8829146fe1c606b6efced2901448eff84e4853ca56']
     }
   }
